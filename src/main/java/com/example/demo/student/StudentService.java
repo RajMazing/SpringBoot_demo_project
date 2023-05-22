7f69bbf1057d8ef9deca06df3cb75837495436d5
RajMazing <caseyraj92@gmail.com>
2023-05-21 21:31:21 -0500

    spelling error in the method call

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -4,13 +4,15 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Service
 public class StudentService {
     private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
 
 
     @Autowired
