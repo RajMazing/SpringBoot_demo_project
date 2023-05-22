4ff853928463415efd53d114a31b6a5f83e43624
RajMazing <caseyraj92@gmail.com>
2023-05-21 23:55:44 -0500

    created StudentConfig class that displays two sample student objects within the postgres database table

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,40 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository ) {
+        return args -> {
+           Student Casey = new Student(
+                    "Casey",
+                    "casey@gmail.com",
+                    LocalDate.of(1992,
+                            Month.JUNE,
+                            10),
+                    30
+            );
+
+            Student Joseph = new Student(
+                    "Joseph",
+                    "joseph@gmail.com",
+                    LocalDate.of(1993,
+                            Month.FEBRUARY,
+                            14),
+                    30
+            );
+
+            repository.saveAll(
+                    List.of(Casey, Joseph)
+                    );
+
+        };
+    }
+}
