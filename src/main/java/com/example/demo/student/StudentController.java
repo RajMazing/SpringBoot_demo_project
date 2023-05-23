94bcc01a4d7d2309f2fd3675ce79ee06f3c4c7df
RajMazing <caseyraj92@gmail.com>
2023-05-22 23:24:30 -0500

    Created an addNewStudent call that adds new students to the server database, if adding new student is successful then it would print to the console.

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,10 +1,7 @@
 package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
@@ -18,10 +15,15 @@ public class StudentController {
 
     @Autowired
     public StudentController(StudentService studentService) {
+
         this.studentService = studentService;
     }
     @GetMapping
     public List<Student> getStudents() {
         return studentService.getStudents();
+    }
+    @PostMapping
+    public void registerNewStudent(@RequestBody  Student student) {
+        studentService.addNewStudent(student);
     }
 }
